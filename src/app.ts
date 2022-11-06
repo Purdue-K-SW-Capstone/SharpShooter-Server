cb1e16079447a75543081e8631b7928311e1b5b2
DongHyeon <skehdgus401@gmail.com>
2022-11-06 18:59:55 -0500

    fix:바이너리 코드 수정

--- src/app.ts
+++ src/app.ts
@@ -46,10 +46,10 @@ wss.on("connection", (ws, req) => {
 });
 
 //이미지 바이너리로 인코딩
-const fs = require('fs');
-let readFile = fs.readFileSync("../pictureTarget.jpg"); //이미지 파일 읽기
-let encode = Buffer.from(readFile).toString('base64'); //파일 인코딩
-let makeEncodeFile = fs.writeFileSync('./encodeFile', encode) //인코딩 파일 만들기
-let readFile2 = fs.readFileSync('./encodeFile', 'base64'); //인코딩된 파일 읽기
-let decode = Buffer.from(encode, 'base64'); //파일 디코딩
-let makeDecodeFile = fs.writeFileSync('./decode.jpg', decode); //디코딩된 파일 만들기
+const fs = require("fs");
+let readFile = fs.readFileSync("img/pictureTarget.jpg"); //이미지 파일 읽기
+let encode = Buffer.from(readFile).toString("base64"); //파일 인코딩
+let makeEncodeFile = fs.writeFileSync("./encodeFile", encode); //인코딩 파일 만들기
+let readFile2 = fs.readFileSync("./encodeFile", "base64"); //인코딩된 파일 읽기
+let decode = Buffer.from(encode, "base64"); //파일 디코딩
+let makeDecodeFile = fs.writeFileSync("./decode.jpg", decode); //디코딩된 파일 만들기
