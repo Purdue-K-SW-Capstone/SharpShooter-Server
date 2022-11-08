a94614626a51c0da6f0ee9d54533dd6da37fd33a
Michael <7471919@naver.com>
2022-11-08 09:37:07 -0500

    fix: modify app.ts logic

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express, { Application } from "express";
-import { WebSocketServer } from "ws";
 import http from "http";
 import path from "path";
 import * as dotenv from "dotenv";
-// import cookieParser from "cookie-parser";
+import fs from "fs";
+import { WebSocketServer } from "ws";
 import router from "./index-router";
+
 
 dotenv.config({ path: path.join(__dirname, "../.env") });
 
@@ -18,53 +19,43 @@ app.use(router);
 const server = http.createServer(app);
 const port = Number(process.env.HTTP_PORT);
 
-//이미지 바이너리로 인코딩
-function jpgTobase64() {
-  const fs = require("fs");
-  let readFile = fs.readFileSync("img/pictureTarget.jpg"); //이미지 파일 읽기
-  let encode = Buffer.from(readFile).toString("base64"); //파일 인코딩
-  return encode;
-}
-function base64ToArrayBuffer(base64: any) {
-  let binary_string = Buffer.from(base64, "base64").toString("binary");
-  let len = binary_string.length;
-  let bytes = new Uint8Array(len);
-  for (let i = 0; i < len; i++) {
-    bytes[i] = binary_string.charCodeAt(i);
-  }
-
-  return bytes.buffer;
-}
-
 server.listen(port, () => {
   console.log(`start! express server on port ${port}`);
 });
 
+// Create new WebSocket Server
 const wss: WebSocketServer = new WebSocketServer({
   port: Number(process.env.WS_PORT),
 });
 
-const content_base64 = encode;
-const binaryData = base64ToArrayBuffer(content_base64);
-
+// 전송받는 데이터 마다 웹소켓 포트를 다르게 해서 서버를 열까?
 wss.on("connection", (ws, req) => {
   console.log(`WS is opened! at port ${process.env.WS_PORT}`);
 
-  // console.log(wss.clients);
   ws.on("message", (data: Buffer) => {
+    // extract the json in binary data(raw data)
+    const res = JSON.parse(data.toString());
+
+    // when get the {start: 1}
+    if ("start" in res) {
+      wss.clients.forEach((client) => {
+        // jpg to byte code for test (temp)
+        const img = fs.readFileSync("./img/pictureTarget.jpg");
+        client.send(img); // send date with type of Blob
+      });
+    } else if ("coordinate" in res) { // {"coordinate": x, "time": x.xx}
+      wss.clients.forEach((client) => {
+        client.send(data);  // send the coordinate(json of string type)
+      });
+    } else if ("image" in res) {  // {"image": Buffer}
+      wss.clients.forEach((client) => {
+        client.send(res?.image);  // send image to client (Blob type)
+      });
+    }
+
+    // This code is for check the Time
     // const time = data.readFloatBE();
     // console.log("time : " + data.readFloatBE());
 
-    const time = data.toString();
-    console.log("get : " + time);
-
-    wss.clients.forEach((client) => {
-      client.send(time);
-    });
   });
 });
-
-// let makeEncodeFile = fs.writeFileSync("./encodeFile", encode); //인코딩 파일 만들기
-// let readFile2 = fs.readFileSync("./encodeFile", "base64"); //인코딩된 파일 읽기
-// let decode = Buffer.from(encode, "base64"); //파일 디코딩
-// let makeDecodeFile = fs.writeFileSync("./decode.jpg", decode); //디코딩된 파일 만들기
