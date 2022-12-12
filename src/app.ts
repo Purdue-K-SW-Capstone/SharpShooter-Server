2d1a8b7b8916e660bf1ad6cbb3cb79fdf93bc154
DongHyeon <skehdgus401@gmail.com>
2022-12-12 18:12:43 -0500

    chore: for test environment

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,10 @@ import path from "path";
 import * as dotenv from "dotenv";
 import fs from "fs";
 import { WebSocketServer } from "ws";
+import router from "./index.router";
+
+// TypeORM
+// import { AppDataSource } from "./ormconfig";
 
 dotenv.config({ path: path.join(__dirname, "../.env") });
 
@@ -12,6 +16,7 @@ const app: Application = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(router);
 
 const server = http.createServer(app);
 const port = Number(process.env.HTTP_PORT);
@@ -19,6 +24,15 @@ const port = Number(process.env.HTTP_PORT);
 server.listen(port, () => {
   console.log(`start! express server on port ${port}`);
 });
+
+//TypeORM
+// AppDataSource.initialize()
+//   .then(() => {
+//     console.log("Data Source has been initialized!");
+//   })
+//   .catch((err) => {
+//     console.log("Error during Data Source initialization : ", err);
+//   });
 
 // Create new WebSocket Server
 const wss: WebSocketServer = new WebSocketServer({
